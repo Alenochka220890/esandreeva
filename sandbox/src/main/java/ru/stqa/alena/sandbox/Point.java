c57798ae70084ca8e30b58562137592cb1e0ae9f
Alenochka220890 <esivanova@list.ru>
2019-03-06 11:37:28 +0300

    конструктор создан

--- sandbox/src/main/java/ru/stqa/alena/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/alena/sandbox/Point.java
@@ -2,19 +2,13 @@ package ru.stqa.alena.sandbox;
 
 public class Point {
 
-  public static void main(String[] args) {
+  public double x;
+  public double y;
 
-    double x1 = 3;
-    double x2 = 9;
-    double y1 = 5;
-    double y2 = 10;
 
-    System.out.println("Дистанция между точками " + distance(x1, x2, y1, y2));
-  }
-
-  public static double distance(double x1,double x2,double y1,double y2) {
-
-    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
 
   }
 }

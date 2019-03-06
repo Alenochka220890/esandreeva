f0f51c6c6d18040a0d278b0cccf287fc65d1497a
Alenochka220890 <esivanova@list.ru>
2019-03-06 15:17:16 +0300

    Задание № 2

--- sandbox/src/main/java/ru/stqa/alena/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/alena/sandbox/MyFirstProgram.java
@@ -2,15 +2,15 @@ package ru.stqa.alena.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-      Point p1 = new Point(3, 4);
-      Point p2 = new Point(5, 6);
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(5, 6);
+    double i = p1.distance(p2);
 
-
-      System.out.println("Дистанция между точками " + distance(p1, p2));
-    }
-      public static double distance(Point p1, Point p2){
-      return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    System.out.println("Дистанция между точками " + distance(p1, p2));
+  }
+  public static double distance(Point p1, Point p2){
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
   }
 }
--- sandbox/src/main/java/ru/stqa/alena/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/alena/sandbox/Point.java
@@ -11,4 +11,9 @@ public class Point {
     this.y = y;
 
   }
+
+  public double distance(Point p2) {
+    return Math.sqrt((p2.x - x) * (p2.x - x) + (p2.y - y) * (p2.y - y));
+
+  }
 }

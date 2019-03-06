5841dcae670d524d4c412c14ad74a2622b9728b2
Alenochka220890 <esivanova@list.ru>
2019-03-06 16:51:32 +0300

    Задание №3

---
+++ sandbox/src/test/java/ru/stqa/alena/sandbox/PointTests_1.java
@@ -0,0 +1,13 @@
+package ru.stqa.alena.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests_1 {
+  @Test
+    public void testDistance (){
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(5, 6);
+    Assert.assertEquals(p1.distance(p2), 1.8284271247461903);
+  }
+}
---
+++ sandbox/src/test/java/ru/stqa/alena/sandbox/PointTests_2.java
@@ -0,0 +1,14 @@
+package ru.stqa.alena.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests_2 {
+
+  @Test
+  public void testDistance() {
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(5, 6);
+    Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
+  }
+}

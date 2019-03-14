6fa465f94feadbd2b946d1974bdfa849db074200
Alenochka220890 <esivanova@list.ru>
2019-03-14 15:14:33 +0300

    дрова получилось сделать

--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/appmanager/ApplicationManager.java
@@ -3,37 +3,39 @@ package ru.stqa.alena.addressbook.tests.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
-  private final NavigationHelper navigationHelper = new NavigationHelper();
+  public WebDriver wd;
+  private NavigationHelper navigationHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
 
   public void init() {
-    navigationHelper.wd = new FirefoxDriver();
-    navigationHelper.wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     login("admin", "secret");
-    contactHelper = new ContactHelper(navigationHelper.wd);
-    groupHelper = new GroupHelper(navigationHelper.wd);
+    contactHelper = new ContactHelper(wd);
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
   }
 
   public void login(String username, String password) {
-    navigationHelper.wd.get("http://localhost/addressbook/group.php");
-    navigationHelper.wd.findElement(By.name("user")).click();
-    navigationHelper.wd.findElement(By.name("user")).clear();
-    navigationHelper.wd.findElement(By.name("user")).sendKeys(username);
-    navigationHelper.wd.findElement(By.name("pass")).clear();
-    navigationHelper.wd.findElement(By.name("pass")).sendKeys(password);
-    navigationHelper.wd.findElement(By.xpath("//input[@value='Login']")).click();
+    wd.get("http://localhost/addressbook/group.php");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   public boolean isElementPresent(By by) {
     try {
-      navigationHelper.wd.findElement(by);
+      wd.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -42,7 +44,7 @@ public class ApplicationManager {
 
   public boolean isAlertPresent() {
     try {
-      navigationHelper.wd.switchTo().alert();
+      wd.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;

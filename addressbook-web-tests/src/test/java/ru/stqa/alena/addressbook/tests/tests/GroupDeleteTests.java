c29f0b0b3644f2f5fad06f1f227a506a3706bd30
Alenochka220890 <esivanova@list.ru>
2019-03-23 09:34:48 +0300

    урок 3.10

--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/GroupDeleteTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/GroupDeleteTests.java
@@ -2,12 +2,16 @@ package ru.stqa.alena.addressbook.tests.tests;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import ru.stqa.alena.addressbook.tests.model.GroupData;
 
 public class GroupDeleteTests extends TestBase {
 
   @Test
   public void testGroupDelete() throws Exception {
     gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteGroup();
     returnToGroupPage();

949822e7beba41e00cc0e12fab8ecd7b0913e1fe
Alenochka220890 <esivanova@list.ru>
2019-03-25 22:42:26 +0300

    Задание 8

--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/ContactModificationTests.java
@@ -8,8 +8,14 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() throws Exception {
       app.getContactHelper().homePagetContact();
+      if (!app.getContactHelper().isThereAContact()) {
+        gotoContactPage();
+        app.getContactHelper().createContact(new ContactData("Yuriy", "Andreev", null, "+79999991122", null));
+      }
       app.getContactHelper().submitContact();
       app.getContactHelper().fillContactForm(new ContactData("Yuriy", "Andreev", "Yura", null, "yurikk170187@yandex.ru"));
       app.getContactHelper().selectContactModification();
     }
-}
+  public void gotoContactPage() {
+    return;
+}}

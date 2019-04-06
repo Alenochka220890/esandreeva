1ccd525fdae338dec4c9ae60881a9248a7e05d25
Alenochka220890 <esivanova@list.ru>
2019-04-06 18:46:19 +0300

    5.8

--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/ContactCreationTests.java
@@ -19,6 +19,18 @@ public class ContactCreationTests extends TestBase {
     Contacts after = app.contact().all();
     assertThat(after.size(),equalTo(before.size() + 1));
     assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));;
+            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  }
+
+  @Test
+  public void testBadContactCreation() throws Exception {
+    app.contact().homePagetContact();
+    Contacts before = app.contact().all();
+    app.goTo().contactPage();
+    ContactData contact = new ContactData().withName("Yuriy'").withSurname("Andreev'");
+    app.contact().create(contact);
+    assertThat(app.contact().count(),equalTo(before.size()));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo(before));
   }
 }

490483255b72ae18ab39f5b4af4e4fa1a2bcb7b6
Alenochka220890 <esivanova@list.ru>
2019-04-07 12:54:50 +0300

    6.2

---
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/generators/ContactDataGenerator.java
@@ -0,0 +1,45 @@
+package ru.stqa.alena.addressbook.tests.generators;
+
+import ru.stqa.alena.addressbook.tests.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  public static void main(String[] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, file);
+  }
+
+  private static void save(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s; %s; %s; %s; %s; %s; %s; %s; %s\n", contact.getFirstname(), contact.getLastname(), contact.getNikname()
+              , contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone(), contact.getEmail(), contact.getEmail2()
+              , contact.getEmail3()));
+    }
+    writer.close();
+  }
+
+  private static List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData().withFirstname(String.format("firstname %s", i))
+              .withLastname(String.format("lastname %s", i)).withNikname(String.format("nickname %s", i))
+              .withHomePhone(String.format("home %s", i)).withMobilePhone(String.format("mobile %s", i))
+              .withWorkPhone(String.format("work %s", i)).withEmail(String.format("email %s", i))
+              .withEmail2(String.format("email2 %s", i)).withEmail3(String.format("email3 %s", i)));
+    }
+    return contacts;
+  }
+
+}

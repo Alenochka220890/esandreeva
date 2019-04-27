cd69df40fca64feab8539833e02f1e0d527ff4a1
Alenochka220890 <esivanova@list.ru>
2019-04-27 17:19:59 +0300

    7.2

--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.alena.addressbook.tests.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.alena.addressbook.tests.model.GroupData;
@@ -18,6 +20,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+  Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
@@ -61,6 +65,7 @@ public class GroupCreationTests extends TestBase {
       Groups after = app.group().all();
       assertThat(after, equalTo(
               before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
     }
 
   @Test
--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/tests/TestBase.java
@@ -1,13 +1,22 @@
 package ru.stqa.alena.addressbook.tests.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.alena.addressbook.tests.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
-  public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+  public static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
@@ -17,5 +26,15 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.contact().stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p){
+    logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("Stop test" + m.getName());
   }
 }

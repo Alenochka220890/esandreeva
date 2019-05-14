ea389d8ca01eadb1ec3e7d7ead7106faa16f3a6e
Alenochka220890 <esivanova@list.ru>
2019-05-14 23:57:18 +0300

    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/alena/addressbook/tests/appmanager/ApplicationManager.java
@@ -1,15 +1,20 @@
 package ru.stqa.alena.addressbook.tests.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoSuchElementException;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -33,13 +38,19 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      sessionHelper.wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      sessionHelper.wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE))
-      sessionHelper.wd = new InternetExplorerDriver();
 
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        sessionHelper.wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        sessionHelper.wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE))
+        sessionHelper.wd = new InternetExplorerDriver();
+    }else{
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      sessionHelper.wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+    }
     sessionHelper.wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     sessionHelper.wd.get(properties.getProperty("web.baseUrl"));
     sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
